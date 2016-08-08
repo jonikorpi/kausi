92d535a95d7a42c4860a1fd57e2f151835df2e75
Joni Korpi <me@jonikorpi.com>
2016-08-08 23:56:12 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'jonikorpi-base-files/reset.css';
+import 'jonikorpi-base-files/classes.css';
+import 'jonikorpi-base-files/main.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
