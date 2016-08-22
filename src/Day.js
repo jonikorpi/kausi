d0284f1c0e5b398c1a9f11c5df6b77dc1d636777
Joni Korpi <me@jonikorpi.com>
2016-08-22 18:00:26 +0300

    Imports debounce back. Oops.

--- src/Day.js
+++ src/Day.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import moment from "moment";
 import classNames from "classnames";
-import { debounce } from 'lodash/function';
+import debounce from "lodash.debounce";
 
 class Day extends Component {
   constructor(props) {
