5169ea37f9f5808090e1854c6b522256c625c88f
Joni Korpi <me@jonikorpi.com>
2016-08-23 10:41:20 +0300

    Links to terms and privacy

--- src/About.js
+++ src/About.js
@@ -11,7 +11,7 @@ class About extends Component {
           To stay in touch, follow <a className="underlined" href="https://twitter.com/jonikorpi">@jonikorpi</a> or visit <a className="underlined" href="http://vuoro.design/">vuoro.design</a>.
         </p>
         <p>
-          The source code can be found on <a className="underlined" href="https://github.com/jonikorpi/kausi/">GitHub</a>.
+          <a className="underlined" href="https://github.com/jonikorpi/kausi/">Source code</a> | <a className="underlined" href="https://github.com/jonikorpi/kausi/blob/master/PRIVACY_POLICY.md">Privacy policy</a> | <a className="underlined" href="https://github.com/jonikorpi/kausi/blob/master/TERMS_OF_SERVICE.md">Terms of service</a>
         </p>
       </div>
     );
