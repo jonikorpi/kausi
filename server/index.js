0b4f3f43c84558ee4bc0c5d5fb2d52d702794282
Joni Korpi <me@jonikorpi.com>
2016-09-18 20:02:20 +0300

    Adds localhost to server console.log

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require('./server');
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-  console.log(`Express is listening on port ${PORT}!`);
+  console.log(`Express is listening on http://localhost:${PORT}`);
 });
