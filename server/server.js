a94b6f8ddeb152140444b9d7ca87cb318759c63c
Joni Korpi <me@jonikorpi.com>
2016-09-18 22:00:20 +0300

    Fixes static resolving issue and geoip for analytics

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const path = require('path');
 const compression = require('compression');
 const morgan = require('morgan');
 const analytics = require('universal-analytics');
-// const expressGeoIP = require('express-geoip');
+const expressGeoIP = require('express-geoip');
 
 const app = express();
+
+// Enable trust
+app.enable('trust proxy');
 
 // Setup gzip
 app.use(compression());
@@ -17,20 +20,20 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] :response-time ms ":met
 app.use(analytics.middleware('UA-3628636-11', {https: true}));
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use("/static", express.static(path.resolve(__dirname, '..', 'build/static')));
 
 // Setup GEOID
-// app.use(expressGeoIP('FI').getCountryCodeMiddleware);
+app.use(expressGeoIP('US').getCountryCodeMiddleware);
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
   req.visitor.pageview({
-    dp: "/",
+    dp: req.originalUrl,
     dh: "https://kausi.xyz",
     dt: "Kausi",
-    uip: req.ip,
-    // geoid: req.countryCode,
+    uip: req.ip || undefined,
+    geoid: req.countryCode || undefined,
   }).send();
 });
 
