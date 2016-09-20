25fc84506550e8886882a57e7c32ef728a8f9311
Joni Korpi <me@jonikorpi.com>
2016-09-20 11:52:48 +0300

    Only track views in production

--- server/server.js
+++ server/server.js
@@ -24,12 +24,17 @@ app.use("/static", express.static(path.resolve(__dirname, '..', 'build/static'))
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-  req.visitor.pageview({
-    dp: req.originalUrl,
-    dh: "https://kausi.xyz",
-    dt: "Kausi",
-    uip: req.ip || undefined,
-  }).send();
+
+  if (process.env.NODE_ENV === "production") {
+    const trackingObject = {
+      dp: req.originalUrl,
+      dh: "https://kausi.xyz",
+      dt: "Kausi",
+      uip: req.ip || undefined,
+    };
+    console.log(trackingObject);
+    req.visitor.pageview(trackingObject).send();
+  }
 });
 
 module.exports = app;
