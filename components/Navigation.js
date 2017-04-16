e7493310bf911f2b02cdb6dc43051ef46586ff76
Joni Korpi <me@jonikorpi.com>
2017-04-16 21:22:07 +0300

    Adds loading state

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,60 +1,118 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Link from "next/link";
+import Router from "next/router";
 
-export default ({ anonymous, uid, url, replaceActiveLinkWith, ...props }) => {
-  const links = [
-    { title: "Timeline", url: "/" },
-    { title: "Lists", url: "/lists" },
-    {
-      title: "Account",
-      url: "/account",
-      hideIf: uid && anonymous,
-    },
-    {
-      title: "Log in/Sign up",
-      url: "/authenticate",
-      hideIf: !uid || (uid && !anonymous),
-    },
-  ];
+export default class Editor extends PureComponent {
+  componentDidMount() {
+    this.mounted = true;
 
-  return (
-    <nav className="navigation padding">
-      <style jsx>
-        {`
-        .navigation {
-          position: absolute;
-          right: 0; top: 0;
-          font-size: 0.625rem;
-          line-height: 0.625rem;
-          z-index: 10;
-          display: flex;
-        }
+    Router.onRouteChangeStart = url => {
+      this.showLoading();
+    };
 
-        .navigation > :global(*) + :global(*) {
-          margin-left: 0.5rem;
-        }
-      `}
-      </style>
+    Router.onRouteChangeComplete = () => {
+      this.hideLoading();
+    };
 
-      {props.children}
+    Router.onRouteChangeError = () => {
+      this.hideLoading();
+    };
+  }
 
-      {links.map((link, index) => {
-        const active = link.url === url.pathname;
+  componentWillUnmount() {
+    this.mounted = false;
+  }
 
-        if (link.hideIf) {
-          return false;
-        } else if (replaceActiveLinkWith && active) {
-          return replaceActiveLinkWith;
-        } else {
-          return (
-            <Link href={link.url} key={index}>
-              <a className={active ? "active" : ""} tabIndex="-1">
-                {link.title}
-              </a>
-            </Link>
-          );
-        }
-      })}
-    </nav>
-  );
-};
+  showLoading = () => {
+    this.mounted && this.setState({ loading: true });
+  };
+
+  hideLoading = () => {
+    this.mounted && this.setState({ loading: false });
+  };
+
+  render() {
+    const { anonymous, uid, url, replaceActiveLinkWith, ...props } = {
+      ...this.props,
+    };
+    const loading = this.state && this.state.loading;
+
+    const links = [
+      { title: "Timeline", url: "/" },
+      { title: "Lists", url: "/lists" },
+      {
+        title: "Account",
+        url: "/account",
+        hideIf: uid && anonymous,
+      },
+      {
+        title: "Log in/Sign up",
+        url: "/authenticate",
+        hideIf: !uid || (uid && !anonymous),
+      },
+    ];
+
+    return (
+      <nav className="navigation padding">
+        <style jsx>{`
+          .navigation {
+            position: absolute;
+            right: 0; top: 0;
+            font-size: 0.625rem;
+            line-height: 0.625rem;
+            z-index: 10;
+            display: flex;
+            align-items: flex-start;
+          }
+
+          .navigation > :global(*) + :global(*) {
+            margin-left: 0.5rem;
+          }
+
+          .spinner {
+            border: 2px dotted;
+            width: 0.625rem;
+            height: 0.625rem;
+            will-change: transform, opacity;
+            animation:
+              spin 1s linear infinite,
+              appear 162ms ease-out
+            ;
+            border-radius: 50%;
+          }
+
+          @keyframes spin {
+            100% { transform: rotate(360deg); }
+          }
+
+          @keyframes appear {
+            0%   { opacity: 0; }
+            100% { opacity: 1; }
+          }
+        `}</style>
+
+        {loading && <div className="spinner" />}
+
+        {props.children}
+
+        {links.map((link, index) => {
+          const active = link.url === url.pathname;
+
+          if (link.hideIf) {
+            return false;
+          } else if (replaceActiveLinkWith && active) {
+            return replaceActiveLinkWith;
+          } else {
+            return (
+              <Link href={link.url} key={index}>
+                <a className={active ? "active" : ""} tabIndex="-1">
+                  {link.title}
+                </a>
+              </Link>
+            );
+          }
+        })}
+      </nav>
+    );
+  }
+}
