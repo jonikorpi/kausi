c3cad6f7df070e90c7e77834602c6cd069129506
Joni Korpi <me@jonikorpi.com>
2017-04-22 01:03:21 +0300

    Adds woff2 preloading

--- components/Head.js
+++ components/Head.js
@@ -14,34 +14,39 @@ export default props => (
       }}
     />
 
-    <link rel="manifest" href={`/static/manifest.json?v1`} />
+    <link
+      rel="preload"
+      as="font"
+      crossorigin="crossorigin"
+      type="font/woff2"
+      href="/static/fonts/inconsolata-v15-latin-ext_latin-regular.woff2?v1"
+    />
+
+    <link rel="manifest" href="/static/manifest.json?v1" />
     {/* <meta name="apple-mobile-web-app-capable" content="yes"/> */}
     <meta name="mobile-web-app-capable" content="yes" />
 
     <link
-      href={`/static/favicon.png?v1`}
+      href="/static/favicon.png?v1"
       rel="shortcut icon"
       type="image/png"
       sizes="any"
     />
     <link
-      href={`/static/favicon.svg?v1`}
+      href="/static/favicon.svg?v1"
       rel="shortcut icon"
       type="image/svg+xml"
       sizes="any"
     />
 
     <link
-      href={`/static/icon.png?v1`}
+      href="/static/icon.png?v1"
       rel="apple-touch-icon-precomposed"
       type="image/png"
       sizes="any"
     />
 
-    <meta
-      content={`https://kausi.xyz/static/icon.png?v1`}
-      property="og:image"
-    />
+    <meta content="https://kausi.xyz/static/icon.png?v1" property="og:image" />
     <meta content="600" property="og:image:width" />
     <meta content="600" property="og:image:height" />
 
