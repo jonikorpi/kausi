cefb333a67bacad0356a21255e2ea27d5fa40a54
Joni Korpi <me@jonikorpi.com>
2017-05-16 18:13:03 +0300

    Adds new rollbar to client

--- helpers/initializeRollbar.js
+++ helpers/initializeRollbar.js
@@ -1,8 +1,8 @@
 const env = (process && process.env && process.env.NODE_ENV) || "development";
 
-if (typeof window !== "undefined" && !window.Rollbar) {
-  window.Rollbar = require("rollbar-browser");
-  window.Rollbar.init({
+if (typeof window !== "undefined" && !window.rollbar) {
+  window.rollbar = require("rollbar");
+  window.Rollbar = new window.rollbar({
     accessToken: "1591caa2cfcf460791065f7c67646373",
     captureUncaught: true,
     captureUnhandledRejections: true,
