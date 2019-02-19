9a19eece23715434cff72e47e412d98fcef5aa80
Joni Korpi <me@jonikorpi.com>
2019-02-19 14:05:17 +0200

    Adds tracking

--- components/Head.js
+++ components/Head.js
@@ -1,56 +1,82 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 
 import globalCSS from "../helpers/globalCSS";
 
-export default props => (
-  <Head>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+export default props => {
+  useEffect(() => {
+    (function(f, a, t, h, o, m) {
+      a[h] =
+        a[h] ||
+        function() {
+          (a[h].q = a[h].q || []).push(arguments);
+        };
+    })(document, window, "//analytics.jonikorpi.com/tracker.js", "fathom");
 
-    <style
-      dangerouslySetInnerHTML={{
-        __html: globalCSS.reset + globalCSS.fontFace + globalCSS.base,
-      }}
-    />
+    if (window.fathom) {
+      window.fathom("set", "siteId", "VJSNU");
+      window.fathom("trackPageview");
+    }
+  }, []);
 
-    <link rel="manifest" href="/static/manifest.json?v1" />
-    {/* <meta name="apple-mobile-web-app-capable" content="yes"/> */}
-    <meta name="mobile-web-app-capable" content="yes" />
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <link
-      href="/static/favicon.png?v1"
-      rel="shortcut icon"
-      type="image/png"
-      sizes="any"
-    />
-    <link
-      href="/static/favicon.svg?v1"
-      rel="shortcut icon"
-      type="image/svg+xml"
-      sizes="any"
-    />
+      <style
+        dangerouslySetInnerHTML={{
+          __html: globalCSS.reset + globalCSS.fontFace + globalCSS.base,
+        }}
+      />
 
-    <link
-      href="/static/icon.png?v1"
-      rel="apple-touch-icon-precomposed"
-      type="image/png"
-      sizes="any"
-    />
+      <link rel="manifest" href="/static/manifest.json?v1" />
+      {/* <meta name="apple-mobile-web-app-capable" content="yes"/> */}
+      <meta name="mobile-web-app-capable" content="yes" />
 
-    <meta content="https://kausi.xyz/static/icon.png?v1" property="og:image" />
-    <meta content="600" property="og:image:width" />
-    <meta content="600" property="og:image:height" />
+      <link
+        href="/static/favicon.png?v1"
+        rel="shortcut icon"
+        type="image/png"
+        sizes="any"
+      />
+      <link
+        href="/static/favicon.svg?v1"
+        rel="shortcut icon"
+        type="image/svg+xml"
+        sizes="any"
+      />
 
-    <title>Kausi</title>
-    <meta content="Kausi" property="og:title" />
+      <link
+        href="/static/icon.png?v1"
+        rel="apple-touch-icon-precomposed"
+        type="image/png"
+        sizes="any"
+      />
 
-    <meta content="A textarea for every day." name="description" />
-    <meta content="A textarea for every day." property="og:description" />
+      <meta
+        content="https://kausi.xyz/static/icon.png?v1"
+        property="og:image"
+      />
+      <meta content="600" property="og:image:width" />
+      <meta content="600" property="og:image:height" />
 
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@jonikorpi" />
+      <title>Kausi</title>
+      <meta content="Kausi" property="og:title" />
 
-    {props.children}
-  </Head>
-);
+      <meta content="A textarea for every day." name="description" />
+      <meta content="A textarea for every day." property="og:description" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@jonikorpi" />
+
+      <script
+        async
+        src="//analytics.jonikorpi.com/tracker.js"
+        id="fathom-script"
+      />
+
+      {props.children}
+    </Head>
+  );
+};
